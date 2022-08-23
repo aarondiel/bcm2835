e11c8513e6cc87e2cd5eef2da7c7e3ca834380a1
aarondiel <aaron.diel@t-online.de>
2022-08-23 14:14:55 +0200

    get peripheral base and size

--- include/bcm2835.h
+++ include/bcm2835.h
@@ -1,3 +1,13 @@
 #pragma once
 
-#define test 1234
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+typedef struct {
+	uint32_t peripheral_base;
+	uint32_t peripheral_size;
+} bcm2835;
+
+bcm2835 *bcm2835_open();
+void bcm2835_close(bcm2835 *chip);
--- src/bcm2835.c
+++ src/bcm2835.c
@@ -1,8 +1,45 @@
 #include "bcm2835.h"
-#include <stdio.h>
+
+void bcm2835_to_little_endian(uint32_t *input) {
+	uint8_t *buffer = (uint8_t *)input;
+
+	*input = (
+		buffer[0] << 24 |
+		buffer[1] << 16 |
+		buffer[2] << 8 |
+		buffer[3]
+	);
+}
+
+bcm2835 *bcm2835_open() {
+	bcm2835 *chip = malloc(sizeof(bcm2835));
+
+	FILE *fp = fopen("/proc/device-tree/soc/ranges", "rb");
+
+	fseek(fp, 4, SEEK_SET);
+	fread(&chip->peripheral_base, sizeof(chip->peripheral_base), 1, fp);
+	bcm2835_to_little_endian(&chip->peripheral_base);
+
+	fseek(fp, 8, SEEK_SET);
+	fread(&chip->peripheral_size, sizeof(chip->peripheral_base), 1, fp);
+	bcm2835_to_little_endian(&chip->peripheral_size);
+
+	fclose(fp);
+
+	return chip;
+}
+
+void bcm2835_close(bcm2835 *chip) {
+	free(chip);
+}
 
 int main(int argc, char **argv) {
-	printf("%d\n", test);
+	bcm2835 *chip = bcm2835_open();
+
+	printf("0x%08x\n", chip->peripheral_base);
+	printf("0x%08x\n", chip->peripheral_size);
+
+	bcm2835_close(chip);
 
 	return 0;
 }
