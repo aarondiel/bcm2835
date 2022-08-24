89509e550e170f2197b5a7f17c958b9c26121b38
aarondiel <aaron.diel@t-online.de>
2022-08-24 08:59:59 +0200

    base peripheral bases

--- include/bcm2835.h
+++ include/bcm2835.h
@@ -1,12 +1,47 @@
 #pragma once
+#pragma pack(1)
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+
+typedef struct {
+	uint32_t function_select[6];
+	uint32_t: 32;
+	uint32_t set[2];
+	uint32_t: 32;
+	uint32_t clear[2];
+	uint32_t: 32;
+	uint32_t level[2];
+	uint32_t: 32;
+	uint32_t event_status[2];
+	uint32_t: 32;
+	u_int32_t rising_edge_detect_enable[2];
+	u_int32_t: 32;
+	u_int32_t falling_edge_detect_enable[2];
+	u_int32_t: 32;
+	u_int32_t high_detect_enable[2];
+	u_int32_t: 32;
+	u_int32_t low_detect_enable[2];
+	u_int32_t: 32;
+	u_int32_t async_rising_edge_detect[2];
+	u_int32_t: 32;
+	u_int32_t async_falling_edge_detect[2];
+	u_int32_t: 32;
+	u_int32_t pull_up_down_enable;
+	u_int32_t pull_up_down_clock[2];
+} bcm2835_gpio_peripherals;
+
+#pragma pack()
 
 typedef struct {
 	uint32_t peripheral_base;
 	uint32_t peripheral_size;
+	bcm2835_gpio_peripherals *gpio;
+	uint32_t *peripherals;
 } bcm2835;
 
 bcm2835 *bcm2835_open();
--- src/bcm2835.c
+++ src/bcm2835.c
@@ -16,15 +16,61 @@ bcm2835 *bcm2835_open() {
 
 	FILE *fp = fopen("/proc/device-tree/soc/ranges", "rb");
 
+	if (fp == NULL) {
+		free(chip);
+		return NULL;
+	}
+
 	fseek(fp, 4, SEEK_SET);
-	fread(&chip->peripheral_base, sizeof(chip->peripheral_base), 1, fp);
+	fread(&chip->peripheral_base, sizeof(&chip->peripheral_base), 1, fp);
 	bcm2835_to_little_endian(&chip->peripheral_base);
 
 	fseek(fp, 8, SEEK_SET);
-	fread(&chip->peripheral_size, sizeof(chip->peripheral_base), 1, fp);
+	fread(&chip->peripheral_size, sizeof(&chip->peripheral_base), 1, fp);
 	bcm2835_to_little_endian(&chip->peripheral_size);
 
 	fclose(fp);
+
+	int fd = open("/dev/mem", O_RDWR);
+
+	if (fp == NULL) {
+		free(chip);
+		return NULL;
+	}
+
+	chip->peripherals = mmap(
+		NULL,
+		chip->peripheral_size,
+		PROT_READ | PROT_WRITE,
+		MAP_SHARED,
+		fd,
+		chip->peripheral_base
+	);
+
+	close(fd);
+
+	if (chip->peripherals == MAP_FAILED) {
+		free(chip);
+		return NULL;
+	}
+
+	chip->gpio = (bcm2835_gpio_peripherals *)chip->peripherals + 0x80000;
+	// chip->pwm = bcm2835_peripherals + 0x83000;
+	// chip->clock  = bcm2835_peripherals + 0x40400;
+	// chip->pads = bcm2835_peripherals + 0x40000;
+	// chip->st = bcm2835_peripherals + 0xc00;
+	// chip->aux = bcm2835_peripherals + 0x85400;
+	// chip->smi = bcm2835_peripherals + 0x180000;
+
+	// chip->spi = {
+	// 	bcm2835_peripherals + 0x81000,
+	// 	bcm2835_peripherals + 0x85420
+	// } ;
+
+	// chip->i2c = {
+	// 	bcm2835_peripherals + 0x81400,
+	// 	bcm2835_peripherals + 0x201000
+	// };
 
 	return chip;
 }
@@ -35,10 +81,6 @@ void bcm2835_close(bcm2835 *chip) {
 
 int main(int argc, char **argv) {
 	bcm2835 *chip = bcm2835_open();
-
-	printf("0x%08x\n", chip->peripheral_base);
-	printf("0x%08x\n", chip->peripheral_size);
-
 	bcm2835_close(chip);
 
 	return 0;
